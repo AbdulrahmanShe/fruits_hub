4cdeae60aa4ab73f48831d1e57a7ecbde9387b99
AbedShehab <abed2003shehab@gmail.com>
2025-12-04 12:17:40 +0200

    first commit

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.fruits_hub"
+    compileSdk = 35
+    ndkVersion = "27.3.13750724"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.fruits_hub"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.11.0")
+}
+}
+
+flutter {
+    source = "../.."
+}
---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/constants.dart';
+import 'package:fruits_hub/core/services/shared_preferences_singleton.dart';
+import 'package:fruits_hub/core/utils/app_images.dart';
+import 'package:fruits_hub/features/auth/presentation/views/sign_in_view.dart';
+import 'package:fruits_hub/features/on_boarding/presentation/views/on_boarding_view.dart';
+import 'package:svg_flutter/svg.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+
+@override
+  void initState() {
+
+    excuteNaviagtion();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [ 
+          SvgPicture.asset(Assets.imagesPlant),
+          ],
+          ),
+        SvgPicture.asset(Assets.imagesLogo),
+        SvgPicture.asset(Assets.imagesSplashBottom,
+        fit: BoxFit.fill,
+        ),
+      ],
+    );
+  }
+  
+  void excuteNaviagtion() {
+    bool  isOnBoardingViewSeen = Prefs.getBool(kIsOnBoardingViewSeen);
+    Future.delayed(const Duration(seconds: 3),(){
+      if (isOnBoardingViewSeen) {
+        // ignore: use_build_context_synchronously
+        Navigator.pushReplacementNamed(context, SignInView.routeName);
+      } else {
+        // ignore: use_build_context_synchronously
+        Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+      }
+      
+    });
+  }
+}
