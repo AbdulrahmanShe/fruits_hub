bdddd099625e33e7399e3d6433a3be3ee6cb541a
AbedShehab <abed2003shehab@gmail.com>
2025-12-07 17:11:21 +0200

    Custome Widgets

--- lib/features/auth/presentation/views/widgets/have_an_account_widget.dart
+++ lib/features/auth/presentation/views/widgets/have_an_account_widget.dart
@@ -2,10 +2,9 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
-import 'package:fruits_hub/features/auth/presentation/views/sign_up_view.dart';
 
-class DontHaveAccountWidget extends StatelessWidget {
-  const DontHaveAccountWidget({super.key});
+class HaveAnAccountWidget extends StatelessWidget {
+  const HaveAnAccountWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,16 +12,16 @@ class DontHaveAccountWidget extends StatelessWidget {
                 TextSpan(
                   children: [
                     TextSpan(
-                      text: 'لا تمتلك حساب؟ ',
+                      text: 'تمتلك حساب بالفعل ؟',
                       style: TextStyles.semiBold16.copyWith(
                         color: const Color(0xFF949D9E),
                           ),
                     ),
                     TextSpan(
                       recognizer: TapGestureRecognizer()..onTap = (){
-                        Navigator.pushNamed(context, SignUpView.routeName);
+                        Navigator.pop(context);
                       },
-                      text: 'قم بإنشاء حساب ',
+                      text: 'تسجيل الدخول',
                       style:
                         TextStyles.semiBold16.copyWith(color: AppColors.primaryColor),
                       ),
@@ -31,4 +30,4 @@ class DontHaveAccountWidget extends StatelessWidget {
                 )
               );
   }
-}
+  }
---
+++ lib/features/auth/presentation/views/widgets/terms_and_conditions.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_colors.dart';
+import 'package:fruits_hub/core/utils/app_text_styles.dart';
+import 'package:fruits_hub/features/auth/presentation/views/widgets/custom_check_box.dart';
+
+class TermsAndConditions extends StatefulWidget {
+  const TermsAndConditions({super.key, required this.onChanged});
+   final ValueChanged<bool> onChanged;
+
+  @override
+  State<TermsAndConditions> createState() => _TermsAndConditionsState();
+}
+
+class _TermsAndConditionsState extends State<TermsAndConditions> {
+   bool isTermsAccepted = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return  
+    // Transform.translate(
+    //   offset: const Offset(14, 0),
+      // child: 
+      Row(
+        children: [
+          // Checkbox(value: false, onChanged: (value){}),
+          CustomCheckBox(
+          onChecked: (value) {
+            isTermsAccepted = value;
+            widget.onChanged(value);
+            setState(() {});
+          },
+          isChecked: isTermsAccepted,
+        ),
+        const SizedBox(
+          width: 16,
+        ),
+
+          Expanded(
+          child: Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: 'من خلال إنشاء حساب ، فإنك توافق على ',
+                  style: TextStyles.semiBold13.copyWith(
+                    color: const Color(0xFF949D9E),
+                  ),
+                ),
+                TextSpan(
+                  text: 'الشروط والأحكام',
+                  style: TextStyles.semiBold13.copyWith(
+                    color: AppColors.lightPrimaryColor,
+                  ),
+                ),
+                const TextSpan(
+                  text: ' ',
+                  style: TextStyles.semiBold13,
+                ),
+                TextSpan(
+                  text: 'الخاصة',
+                  style: TextStyles.semiBold13.copyWith(
+                    color: AppColors.lightPrimaryColor,
+                  ),
+                ),
+                const TextSpan(
+                  text: ' ',
+                  style: TextStyles.semiBold13,
+                ),
+                TextSpan(
+                  text: 'بنا',
+                  style: TextStyles.semiBold13
+                      .copyWith(color: AppColors.lightPrimaryColor),
+                ),
+              ],
+            ),
+          ),
+          ),
+          
+        ],
+      // ),
+    );
+  }
+}
