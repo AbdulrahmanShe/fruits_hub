49a68f89288b031aa437876abda1a902fb862847
AbedShehab <abed2003shehab@gmail.com>
2025-12-07 17:15:22 +0200

    refactor code

--- lib/core/widgets/custom_text_form_field.dart
+++ lib/core/widgets/custom_text_form_field.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fruits_hub/core/constants.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 
 class CustomTextFormField extends StatelessWidget {
