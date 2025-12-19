fad4517e67c929112147df3728deac4356dac577
AbedShehab <abed2003shehab@gmail.com>
2025-12-19 15:15:02 +0200

    create page route

---
+++ lib/core/routing/app_pages.dart
@@ -0,0 +1,32 @@
+import 'package:get/get.dart';
+import 'package:fruits_hub/features/auth/bindings/auth_binding.dart';
+import 'package:fruits_hub/features/auth/presentation/views/sign_in_view.dart';
+import 'package:fruits_hub/features/auth/presentation/views/sign_up_view.dart';
+import 'package:fruits_hub/features/on_boarding/presentation/views/on_boarding_view.dart';
+import 'package:fruits_hub/features/splash/presentation/views/splash_view.dart';
+
+class AppPages {
+  static final routes = [
+    GetPage(
+      name: SplashView.routeName,
+      page: () => const SplashView(),
+    ),
+
+    GetPage(
+      name: SignInView.routeName,
+      page: () => const SignInView(),
+      binding: AuthBinding(), // 👈 هنا السحر
+    ),
+
+    GetPage(
+      name: SignUpView.routeName,
+      page: () => const SignUpView(),
+      binding: AuthBinding(), // 👈 وهنا
+    ),
+
+    GetPage(
+      name: OnBoardingView.routeName,
+      page: () => const OnBoardingView(),
+    ),
+  ];
+}
