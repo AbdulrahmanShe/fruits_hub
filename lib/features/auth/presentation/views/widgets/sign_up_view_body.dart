78cee709ed783ba4b627080942ecabc66fb40acb
AbedShehab <abed2003shehab@gmail.com>
2025-12-19 15:23:07 +0200

    create SignUpViewBody to Getx

--- lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/constants.dart';
 import 'package:fruits_hub/core/widgets/custom_button.dart';
 import 'package:fruits_hub/core/widgets/custom_text_form_field.dart';
+import 'package:fruits_hub/features/auth/presentation/controller/auth_controller.dart';
+import 'package:fruits_hub/features/auth/presentation/views/sign_in_view.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/have_an_account_widget.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/terms_and_conditions.dart';
+import 'package:get/get.dart';
 
 class SignUpViewBody extends StatefulWidget {
   const SignUpViewBody({super.key});
@@ -13,6 +16,7 @@ class SignUpViewBody extends StatefulWidget {
 }
 
 class _SignUpViewBodyState extends State<SignUpViewBody> {
+  final AuthController controller = Get.find<AuthController>();
   late String email, userName, password;
   late bool isTermsAccepted = false;
 
@@ -24,14 +28,29 @@ class _SignUpViewBodyState extends State<SignUpViewBody> {
         child: Column(
           children: [
             SizedBox(height: 24,),
-            CustomTextFormField(hintText: 'الإسم كامل', textInputType: TextInputType.name),
+            CustomTextFormField(
+              hintText: 'الإسم كامل', 
+              textInputType: TextInputType.name,
+                onChanged: (value) {
+                  userName = value;
+                },
+              ),
             SizedBox(height: 16,),
-            CustomTextFormField(hintText: 'البريد الإلكتروني', textInputType: TextInputType.emailAddress),
+            CustomTextFormField(
+              hintText: 'البريد الإلكتروني', 
+              textInputType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  email = value;
+                },
+              ),
             SizedBox(height: 16,),
             CustomTextFormField(
                   suffixIcon: Icon(Icons.remove_red_eye,color: Color(0xffC9CECF),),
                   hintText: 'كلمة المرور', 
-                  textInputType: TextInputType.visiblePassword
+                  textInputType: TextInputType.visiblePassword,
+                    onChanged: (value) {
+                      password = value;
+                    },
                   ),
             SizedBox(height: 16,),
             TermsAndConditions(
@@ -40,7 +59,35 @@ class _SignUpViewBodyState extends State<SignUpViewBody> {
                 },
                 ),
             SizedBox(height: 30,),
-          CustomButton(onPressed: (){}, text: 'إنشاء حساب جديد'),
+          Obx((){
+            return CustomButton(
+              text: controller.isLoading.value
+                  ? 'جاري إنشاء الحساب...'
+                  : 'إنشاء حساب جديد',
+              onPressed: controller.isLoading.value 
+              ? (){}
+              : () async {
+                if (!isTermsAccepted) {
+                  Get.snackbar(
+                    'تنبيه',
+                     'يجب الموافقة على الشروط والأحكام',
+                     snackPosition: SnackPosition.BOTTOM,
+                     );
+                     return;
+                }
+                bool success = await controller.signUpWithEmailAndPassword(
+            email: email, 
+            password: password, 
+            name: userName,
+          );
+          if (success) {
+            Get.toNamed(SignInView.routeName);
+          }
+              }
+              
+              );
+              },
+              ),
           const SizedBox(
                 height: 26,
               ),
