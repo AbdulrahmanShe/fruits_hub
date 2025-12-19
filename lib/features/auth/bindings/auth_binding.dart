bcd71acd319c7c6bc9ca3f35f4673646077124d1
AbedShehab <abed2003shehab@gmail.com>
2025-12-19 15:19:45 +0200

    create AuthBinding

---
+++ lib/features/auth/bindings/auth_binding.dart
@@ -0,0 +1,17 @@
+import 'package:fruits_hub/core/services/firebase_auth_service.dart';
+import 'package:fruits_hub/features/auth/data/repo/auth_repo_impl.dart';
+import 'package:fruits_hub/features/auth/presentation/controller/auth_controller.dart';
+import 'package:get/get.dart';
+import 'package:fruits_hub/features/auth/domin/repos/auth_repo.dart';
+class AuthBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => FirebaseAuthService());
+    Get.lazyPut<AuthRepo>(
+      () => AuthRepoImpl(firebaseAuthService: Get.find()),
+    );
+    Get.lazyPut(
+      () => AuthController(authRepo: Get.find()),
+    );
+  }
+}
