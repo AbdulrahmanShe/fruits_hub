1afe89bbbbf55373a46df3204487458c01f1aaff
AbedShehab <abed2003shehab@gmail.com>
2025-12-21 21:07:35 +0200

    add signInWithEmailAndPassword in AuthRepoImpl

--- lib/features/auth/data/repo/auth_repo_impl.dart
+++ lib/features/auth/data/repo/auth_repo_impl.dart
@@ -28,5 +28,21 @@ class AuthRepoImpl extends AuthRepo{
 }
    
   }
+  
+  @override
+  Future<Either<Failure, UserEntity>> signInWithEmailAndPassword(
+    String email, String password,) async{
+      try {
+  var user = await firebaseAuthService.signInWithEmailAndPassword(
+   email: email, password: password,);
+   return right(
+    UserModel.fromFirebaseUser(user),
+   );
+} on CustomException catch (e) {
+  return left(ServerFailure(e.message));
+}catch (e) {
+  return left(ServerFailure(e.toString()));
+}
+  }
 
 }
