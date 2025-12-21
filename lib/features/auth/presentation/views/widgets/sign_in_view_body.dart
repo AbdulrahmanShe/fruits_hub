6dd3813215eb07ebfe7c07dcdd4653ec37484e7e
AbedShehab <abed2003shehab@gmail.com>
2025-12-21 21:11:05 +0200

    add logic to SignInViewBody

--- lib/features/auth/presentation/views/widgets/sign_in_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_in_view_body.dart
@@ -7,12 +7,29 @@ import 'package:fruits_hub/core/utils/app_images.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 import 'package:fruits_hub/core/widgets/custom_button.dart';
 import 'package:fruits_hub/core/widgets/custom_text_form_field.dart';
+import 'package:fruits_hub/core/widgets/password_field.dart';
+import 'package:fruits_hub/features/auth/presentation/controller/auth_controller.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/dont_have_account_widget.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/or_divider.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/social_login_button.dart';
+import 'package:fruits_hub/features/home/presentation/views/home_view.dart';
+import 'package:get/get.dart';
 
-class SignInViewBody extends StatelessWidget {
+class SignInViewBody extends StatefulWidget {
   const SignInViewBody({super.key});
+
+  @override
+  State<SignInViewBody> createState() => _SignInViewBodyState();
+}
+
+class _SignInViewBodyState extends State<SignInViewBody> {
+
+  final AuthController controller = Get.find<AuthController>();
+  late String email, password;
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>(); 
+
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
 
   @override
   Widget build(BuildContext context) {
@@ -21,22 +38,26 @@ class SignInViewBody extends StatelessWidget {
         padding: const EdgeInsets.symmetric(
           horizontal: kHorizintalPadding
         ),
-        child: SingleChildScrollView(
-          // scrollDirection: Axis.vertical,
-          // clipBehavior: Clip.none,
+        child: Form(
+          key: formKey,
+          autovalidateMode: autovalidateMode,
           child: Column(
             children: [
               SizedBox(height: 24,),
               CustomTextFormField(
                 hintText: 'البريد الالكتروني', 
-                textInputType: TextInputType.emailAddress
+                textInputType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  email = value;
+                },
                 ),
                 SizedBox(height: 16,),
-              CustomTextFormField(
-                suffixIcon: Icon(Icons.remove_red_eye,color: Color(0xffC9CECF),),
-                hintText: 'كلمة المرور', 
-                textInputType: TextInputType.visiblePassword
-                ),
+              PasswordField(
+                obscureText: true,
+                onChanged: (value){
+                  password = value;
+                },
+              ),
                 const SizedBox(
                     height: 16,
                   ),
@@ -54,16 +75,40 @@ class SignInViewBody extends StatelessWidget {
                   const SizedBox(
                     height: 33,
                   ),
-                  CustomButton(
-                    onPressed: () {
-        
-                    },
-                    text: 'تسجيل دخول',
+                  Obx((){
+                    return CustomButton(
+                                  
+                      text: controller.isLoading.value 
+                      ? 'جاري تسجيل الدخول...' 
+                      : 'تسجيل دخول',
+                                  
+                      onPressed: controller.isLoading.value 
+                                  ? (){return;}
+                                  : () async { 
+                                    if (!formKey.currentState!.validate()) {
+                      setState(() => autovalidateMode = AutovalidateMode.always);
+                      return;
+                    }
+                    
+                    formKey.currentState!.save();
+                                  
+                    bool success = await controller.signInWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    );
+                                  
+                    if (success) {
+                      Get.toNamed(HomeView.routeName);
+                    }
+                                    },
+                      
+                    );
+                  }
                   ),
                   const SizedBox(
                     height: 33,
                   ),
-        
+                  
                    DontHaveAccountWidget(),
                   
                   const SizedBox(
@@ -75,7 +120,7 @@ class SignInViewBody extends StatelessWidget {
                   ),
                   SocialLoginButton(
                   onPressed: () {
-      
+                
                   },
                   image: Assets.imagesGoogleIcon,
                   title: 'تسجيل بواسطة جوجل',
@@ -88,7 +133,7 @@ class SignInViewBody extends StatelessWidget {
                         children: [
                           SocialLoginButton(
                             onPressed: () {
-      
+                
                             },
                             image: Assets.imagesApplIcon,
                             title: 'تسجيل بواسطة أبل',
@@ -112,6 +157,4 @@ class SignInViewBody extends StatelessWidget {
       ),
     );
   }
-
-  
 }
