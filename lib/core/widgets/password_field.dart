698afc966969f4c26e8312a2c94701251e596930
AbedShehab <abed2003shehab@gmail.com>
2025-12-23 16:13:49 +0200

    add validator to PasswordField

--- lib/core/widgets/password_field.dart
+++ lib/core/widgets/password_field.dart
@@ -3,11 +3,12 @@ import 'package:fruits_hub/core/widgets/custom_text_form_field.dart';
 
 class PasswordField extends StatefulWidget {
   const PasswordField({
-    super.key, this.onChanged, this.obscureText = true,
+    super.key, this.onChanged, this.obscureText = true, this.hintText = 'كلمة المرور',
   });
 
   final void Function(String)? onChanged;
   final bool obscureText;
+  final String hintText;
 
   @override
   State<PasswordField> createState() => _PasswordFieldState();
@@ -16,18 +17,30 @@ class PasswordField extends StatefulWidget {
 class _PasswordFieldState extends State<PasswordField> {
 
 late bool obscureText;
+late String hintText;
 
 @override
   void initState() {
     super.initState();
     obscureText = widget.obscureText;
+    hintText = widget.hintText;
   }
 
   @override
   Widget build(BuildContext context) {
     return CustomTextFormField(
+      validator: (value) {
+  if (value == null || value.isEmpty) {
+    return 'كلمة المرور مطلوبة';
+  }
+  if (value.length < 6) {
+    return 'كلمة المرور يجب أن تكون 6 أحرف على الأقل';
+  }
+  return null;
+},
+
       obscureText: obscureText,
-       hintText: 'كلمة المرور', 
+       hintText: hintText, 
           textInputType: TextInputType.visiblePassword,
             onChanged: widget.onChanged,
           suffixIcon: GestureDetector(
