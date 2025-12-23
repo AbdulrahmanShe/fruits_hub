c4451bf28298a6b6c944a4b180428839b853c2ee
AbedShehab <abed2003shehab@gmail.com>
2025-12-23 16:20:59 +0200

    add sendPasswordResetEmail to AuthRepo

--- lib/features/auth/domin/repos/auth_repo.dart
+++ lib/features/auth/domin/repos/auth_repo.dart
@@ -3,12 +3,25 @@ import 'package:fruits_hub/core/errors/failures.dart';
 import 'package:fruits_hub/features/auth/domin/entites/user_entity.dart';
 
 abstract class AuthRepo {
+
+  // Register
   Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
-      String email, String password, String name);
+    String email,
+    String password,
+    String name,
+  );
 
-       // login
-
+  // Login
   Future<Either<Failure, UserEntity>> signInWithEmailAndPassword(
-      String email, String password,);
+    String email,
+    String password,
+  );
+
+  // ===== Password Reset =====
+
+  /// Email reset
+  Future<Either<Failure, Unit>> sendPasswordResetEmail(
+    String email,
+  );
 
 }
