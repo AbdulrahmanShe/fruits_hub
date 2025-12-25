f19df3bae00ff6757b634a71ed25df93a082f23a
AbedShehab <abed2003shehab@gmail.com>
2025-12-25 19:56:54 +0200

    add getUserData in DatabaseService

--- lib/core/services/database_service.dart
+++ lib/core/services/database_service.dart
@@ -1,7 +1,14 @@
+import 'package:fruits_hub/features/auth/domin/entites/user_entity.dart';
+
 abstract class DatabaseService{
 //collectionName  or path => General Name (collectionName or API)
   Future<void> addData({
     required String collectionName, 
     required Map<String, dynamic> data
     });
+
+    Future<UserEntity> getUserData({
+      required String collectionName,
+      required String uid,
+      });
 }
