926985c0831853a20251548391edfc8310a538fb
AbedShehab <abed2003shehab@gmail.com>
2025-12-25 22:33:06 +0200

    add checkIfDataExists in DatabaseService

--- lib/core/services/database_service.dart
+++ lib/core/services/database_service.dart
@@ -2,11 +2,17 @@ abstract class DatabaseService{
 //collectionName  or path => General Name (collectionName or API)
   Future<void> addData({
     required String collectionName, 
-    required Map<String, dynamic> data
+    required Map<String, dynamic> data,
+    String? docuementId,
     });
 
     Future<Map<String, dynamic>> getData({
       required String collectionName,
       required String docuementId,
       });
+
+      Future<bool> checkIfDataExists({
+        required String collectionName, 
+        required String docuementId
+        });
 }
