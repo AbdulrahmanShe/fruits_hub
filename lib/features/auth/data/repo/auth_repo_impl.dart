391d94ff8d9ba46518ecbff19f624d7b65cb16b2
AbedShehab <abed2003shehab@gmail.com>
2025-12-29 22:57:06 +0200

    add saveUserData

--- lib/features/auth/data/repo/auth_repo_impl.dart
+++ lib/features/auth/data/repo/auth_repo_impl.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fruits_hub/core/constants.dart';
 import 'package:fruits_hub/core/errors/exceptions.dart';
 import 'package:fruits_hub/core/errors/failures.dart';
 import 'package:fruits_hub/core/services/database_service.dart';
 import 'package:fruits_hub/core/services/firebase_auth_service.dart';
+import 'package:fruits_hub/core/services/shared_preferences_singleton.dart';
 import 'package:fruits_hub/core/utils/backend_endpoint.dart';
 import 'package:fruits_hub/features/auth/data/models/user_model.dart';
 import 'package:fruits_hub/features/auth/domin/entites/user_entity.dart';
@@ -56,6 +60,7 @@ class AuthRepoImpl extends AuthRepo{
   var user = await firebaseAuthService.signInWithEmailAndPassword(
    email: email, password: password,);
    var userEntity = await getUserData(uid: user.uid);
+   await saveUserData(user: userEntity);
    return right(
     userEntity,
    );
@@ -114,7 +119,7 @@ Future<Either<Failure, UserEntity>> signInWithGoogle() async {
   Future addUserData({required UserEntity user}) async{
     await databaseService.addData(
       collectionName: BackendEndpoint.addUserData, 
-      data: user.toMap(),
+      data: UserModel.fromEntity(user).toMap(),
       docuementId: user.uId,
       );
   }
@@ -126,6 +131,13 @@ Future<Either<Failure, UserEntity>> signInWithGoogle() async {
       docuementId: uid,
       );
       return UserModel.fromJson(userData);
+  }
+  
+  @override
+  Future saveUserData({required UserEntity user}) {
+    var jsonData = jsonEncode(UserModel.fromEntity(user).toMap());
+
+    return Prefs.setString(kUserData, jsonData);
   }
 
 }
