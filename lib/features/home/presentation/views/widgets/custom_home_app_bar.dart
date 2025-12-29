a0e0fa7834dc0fde8cf444712f8bcf9e348d8827
AbedShehab <abed2003shehab@gmail.com>
2025-12-29 21:50:00 +0200

    build NotificationWidget

--- lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
@@ -1,22 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/utils/app_images.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
-import 'package:svg_flutter/svg.dart';
+import 'package:fruits_hub/core/widgets/notification_widget.dart';
 class CustomHomeAppBar extends StatelessWidget {
   const CustomHomeAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      // trailing: const NotificationWidget(),
-      trailing: Container(
-        padding: EdgeInsets.all(12),
-        decoration: ShapeDecoration(
-          color: Color(0xFFEEF8ED),
-          shape: OvalBorder()
-          ),
-          child: SvgPicture.asset(Assets.imagesNotification),
-      ),
+      trailing: const NotificationWidget(),
       leading: Image.asset(Assets.imagesProfileImage),
       title: Text(
         'صباح الخير !..',
