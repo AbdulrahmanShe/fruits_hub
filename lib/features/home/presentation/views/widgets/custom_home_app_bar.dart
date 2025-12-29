7de6a05a3250cc0dc023531425eab7681ea041e0
AbedShehab <abed2003shehab@gmail.com>
2025-12-29 22:55:54 +0200

    getUser to UserEntity

--- lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/helper_functions/get_user.dart';
 import 'package:fruits_hub/core/utils/app_images.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 import 'package:fruits_hub/core/widgets/notification_widget.dart';
@@ -18,8 +19,7 @@ class CustomHomeAppBar extends StatelessWidget {
         ),
       ),
       subtitle: Text(
-        // getUser().name,
-        'Mustafa',
+        getUser().name,
         textAlign: TextAlign.right,
         style: TextStyles.bold16,
       ),
