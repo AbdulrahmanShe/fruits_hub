f3b1b249e6c46a8e2b41b2f01ec5096d389b52e7
AbedShehab <abed2003shehab@gmail.com>
2026-01-08 01:00:29 +0200

    add ProductsController in HomeViewBody and called initState() => controller.getBestSellingProducts()

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/constants.dart';
+import 'package:fruits_hub/core/controller/products_controller.dart';
 import 'package:fruits_hub/core/widgets/search_text_field.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/best_selling_header.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/featured_list.dart';
-import 'package:fruits_hub/features/home/presentation/views/widgets/products_grid_view.dart';
+import 'package:fruits_hub/features/home/presentation/views/widgets/products_grid_view_controller.dart';
+import 'package:get/get.dart';
 
 class HomeViewBody extends StatefulWidget {
-  const HomeViewBody({super.key});
+  const HomeViewBody({
+    super.key,
+  });
 
   @override
   State<HomeViewBody> createState() => _HomeViewBodyState();
 }
 
 class _HomeViewBodyState extends State<HomeViewBody> {
+  final ProductsController controller = Get.find<ProductsController>();
+  
+  @override
+  void initState() {
+    super.initState();
+    controller.getBestSellingProducts();
+  }
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -37,9 +48,10 @@ class _HomeViewBodyState extends State<HomeViewBody> {
               ],
             ),
           ),
-          ProductsGridView(),
+          ProductsGridViewController(),
         ],
       ),
     );
   }
 }
+
