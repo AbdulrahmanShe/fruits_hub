d95d41d7fb145eca2dc95e79519cb32d43c7b2d7
AbedShehab <abed2003shehab@gmail.com>
2026-01-08 00:48:49 +0200

    add ProductsViewBody

---
+++ lib/features/home/presentation/views/widgets/products_view_body.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/constants.dart';
+import 'package:fruits_hub/core/controller/products_controller.dart';
+import 'package:fruits_hub/core/widgets/custom_app_bar_notification.dart';
+import 'package:fruits_hub/core/widgets/search_text_field.dart';
+import 'package:fruits_hub/features/home/presentation/views/widgets/products_grid_view_controller.dart';
+import 'package:fruits_hub/features/home/presentation/views/widgets/products_view_header.dart';
+import 'package:get/get.dart';
+
+class ProductsViewBody extends StatefulWidget {
+  const ProductsViewBody({super.key});
+
+  @override
+  State<ProductsViewBody> createState() => _ProductsViewBodyState();
+}
+
+class _ProductsViewBodyState extends State<ProductsViewBody> {
+  final ProductsController controller = Get.find<ProductsController>();
+  
+  @override
+  void initState() {
+    super.initState();
+    controller.getProducts();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: kTopPaddding,
+                ),
+                buildAppBarNotification( 
+                  title: 'المنتجات',
+                  showBackBottom: false,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const SearchTextField(),
+                const SizedBox(
+                  height: 12,
+                ),
+                ProductsViewHeader(
+                    productsLength: 
+                    controller.productsLength
+                        // context.read<ProductsCubit>().productsLength
+                        ),
+                const SizedBox(
+                  height: 8,
+                ),
+              ],
+            ),
+          ),
+          const ProductsGridViewController(),
+        ],
+      ),
+    );
+  }
+}
