d7f906153b15a3c4dd30366c42a6db5ef12b6e5b
AbedShehab <abed2003shehab@gmail.com>
2026-01-08 22:11:03 +0200

    add CartItemEntity

---
+++ lib/features/home/domain/entites/car_item_entity.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+import 'package:fruits_hub/core/entities/product_entity.dart';
+import 'package:get/get.dart';
+
+class CartItemEntity extends Equatable{
+  final ProductEntity productEntity;
+  final RxInt quantity;
+
+  CartItemEntity({required this.productEntity, int quantity = 0})
+      : quantity = quantity.obs;
+
+  num calculateTotalPrice() => productEntity.price * quantity.value;
+
+  num calculateTotalWeight() => productEntity.unitAmount * quantity.value;
+
+  increaseQuantity() => quantity.value++;
+  decreasQuantity() {
+  if (quantity.value > 0) quantity.value--;
+}
+
+  @override
+  List<Object?> get props => [productEntity];
+}
