f6a6aaab7e5d3af4703531c63577d9af1df4230d
AbedShehab <abed2003shehab@gmail.com>
2026-01-08 22:07:54 +0200

    add GlobalBinding to CartController

---
+++ lib/core/bindings/global_binding.dart
@@ -0,0 +1,12 @@
+import 'package:fruits_hub/features/home/presentation/controller/cart_controller.dart';
+import 'package:get/get.dart';
+
+class GlobalBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.put<CartController>(
+      CartController(),
+      permanent: true,
+    );
+  }
+}
