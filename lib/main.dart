19459dde9917f6350d8252ac6af295e7826d3097
AbedShehab <abed2003shehab@gmail.com>
2026-01-08 22:07:29 +0200

    add initialBinding

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:fruits_hub/core/bindings/global_binding.dart';
 import 'package:fruits_hub/core/services/shared_preferences_singleton.dart';
 import 'package:fruits_hub/features/splash/presentation/views/splash_view.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
@@ -24,6 +25,7 @@ class FruitHub extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      initialBinding: GlobalBinding(),
 
       // 🎨 Theme
       theme: ThemeData(
@@ -53,3 +55,11 @@ class FruitHub extends StatelessWidget {
     );
   }
 }
+    // Folow 
+// UI ,
+// Entity , 
+//Repo , Repo Imp , 
+//DatabaseServices , FirebaseServices , 
+//Controller , 
+//Finish Logic, 
+//Finish Featuers
