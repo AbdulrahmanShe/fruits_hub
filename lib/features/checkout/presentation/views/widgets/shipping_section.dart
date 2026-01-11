6b125f8312780d13bc69aee03aa0ffb82a1022c3
AbedShehab <abed2003shehab@gmail.com>
2026-01-11 23:45:57 +0200

    create Shipping Section

---
+++ lib/features/checkout/presentation/views/widgets/shipping_section.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/checkout/presentation/controller/checkout_controller.dart';
+import 'package:fruits_hub/features/checkout/presentation/views/widgets/shipping_item.dart';
+import 'package:get/get.dart';
+
+class ShippingSection extends StatefulWidget {
+  const ShippingSection({super.key});
+
+  @override
+  State<ShippingSection> createState() => _ShippingSectionState();
+}
+
+class _ShippingSectionState extends State<ShippingSection> with AutomaticKeepAliveClientMixin{
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    final CheckoutController controller = Get.find<CheckoutController>();
+    return Column(
+      children: [
+        const SizedBox(
+          height: 33,
+        ),
+
+        ShippingItem(
+          onTap: (){
+          controller.selectedShippingIndex.value = 0;
+            setState(() {});
+            controller.orderEntity.payWithCash = true;
+          },
+          
+          isSelected: controller.selectedShippingIndex.value == 0,
+          title: 'الدفع عند الاستلام',
+          subTitle: 'التسليم من المكان',
+          price: (controller.cartTotal + 30).toStringAsFixed(0),
+        ),
+
+        const SizedBox(
+          height: 16,
+        ),
+
+        ShippingItem(
+          onTap: (){
+          controller.selectedShippingIndex.value = 1;
+            setState(() {});
+            controller.orderEntity.payWithCash = false;
+          },
+          isSelected: controller.selectedShippingIndex.value == 1,
+          title: 'الدفع اونلاين',
+          subTitle: 'يرجي تحديد طريقه الدفع',
+          price: controller.cartTotal.toStringAsFixed(0),
+        ),
+      ],
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
