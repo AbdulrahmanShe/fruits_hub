0f161d37a3f54834555e587e72cd900008043f03
AbedShehab <abed2003shehab@gmail.com>
2026-01-11 17:54:36 +0200

    create Payment Section

---
+++ lib/features/checkout/presentation/views/widgets/payment_section.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PaymentSection extends StatelessWidget {
+  const PaymentSection({super.key, required this.pageController});
+
+  final PageController pageController;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 24,
+        ),
+        
+      ],
+    );
+  }
+}
