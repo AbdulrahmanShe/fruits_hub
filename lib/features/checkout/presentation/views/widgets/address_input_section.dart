3e2e342886907cabe8f80cc07bfdd98facd5dd3e
AbedShehab <abed2003shehab@gmail.com>
2026-01-11 17:41:13 +0200

    create Address Input Section

---
+++ lib/features/checkout/presentation/views/widgets/address_input_section.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/widgets/custom_text_form_field.dart';
+
+class AddressInputSection extends StatelessWidget {
+  const AddressInputSection(
+      {super.key, required this.formKey, required this.valueListenable});
+
+  final GlobalKey<FormState> formKey;
+  final ValueListenable<AutovalidateMode> valueListenable;
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: ValueListenableBuilder<AutovalidateMode>(
+        valueListenable: valueListenable,
+        builder: (context, value, child) => Form(
+          key: formKey,
+          autovalidateMode: value,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 24,
+              ),
+              CustomTextFormField(
+                onChanged: (value) {},
+                hintText: 'الاسم كامل',
+                textInputType: TextInputType.text,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              CustomTextFormField(
+                onChanged: (value) {},
+                hintText: 'البريد الإلكتروني',
+                textInputType: TextInputType.text,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              CustomTextFormField(
+                onChanged: (value) {},
+                hintText: 'العنوان',
+                textInputType: TextInputType.text,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              CustomTextFormField(
+                onChanged: (value) {},
+                hintText: 'المدينه',
+                textInputType: TextInputType.text,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              CustomTextFormField(
+                onChanged: (value) {},
+                hintText: 'رقم الطابق , رقم الشقه ..',
+                textInputType: TextInputType.text,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              CustomTextFormField(
+                onChanged: (value) {},
+                hintText: 'رقم الهاتف',
+                textInputType: TextInputType.number,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
