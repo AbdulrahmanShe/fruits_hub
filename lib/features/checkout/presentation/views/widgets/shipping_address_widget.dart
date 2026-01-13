a3dad7ee1b741544b41f20992da5242407db449c
AbedShehab <abed2003shehab@gmail.com>
2026-01-13 18:18:28 +0200

    Create ShippingAddressWidget

---
+++ lib/features/checkout/presentation/views/widgets/shipping_address_widget.dart
@@ -0,0 +1,68 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_images.dart';
+import 'package:fruits_hub/features/checkout/presentation/controller/checkout_controller.dart';
+import 'package:fruits_hub/features/checkout/presentation/views/widgets/payment_item.dart';
+import 'package:get/get.dart';
+import 'package:svg_flutter/svg_flutter.dart';
+import '../../../../../core/utils/app_text_styles.dart';
+
+class ShippingAddressWidget extends StatelessWidget {
+  const ShippingAddressWidget({
+    super.key,
+    required this.pageController,
+  });
+  
+  final PageController pageController;
+  @override
+  Widget build(BuildContext context) {
+    final CheckoutController controller = Get.find<CheckoutController>();
+    log(controller.orderEntity.toString());
+    return PaymentItem(
+      tile: 'عنوان التوصيل',
+      child: Row(
+        children: [
+          SvgPicture.asset(
+            Assets.imagesLocation,
+          ),
+          const SizedBox(
+            width: 8,
+          ),
+          Text(
+            ' ${controller.orderEntity.shippingAddressEntity}',
+            textAlign: TextAlign.right,
+            style: TextStyles.regular13.copyWith(
+              color: const Color(0xFF4E5556),
+            ),
+          ),
+          const Spacer(),
+          GestureDetector(
+            onTap: () {
+              pageController.animateToPage(1,
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeIn);
+            },
+            child: SizedBox(
+              child: Row(
+                children: [
+                  SvgPicture.asset(
+                    Assets.imagesEdit,
+                  ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  Text(
+                    'تعديل',
+                    style: TextStyles.semiBold13.copyWith(
+                      color: const Color(0xFF949D9E),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
