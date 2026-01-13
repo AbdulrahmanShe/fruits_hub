db79a73dc2e2e48a1783ade486250de0fb931c3f
AbedShehab <abed2003shehab@gmail.com>
2026-01-13 19:59:07 +0200

    add Repo and controller to CheckoutBinding

--- lib/features/checkout/binding/checkout_binding .dart
+++ lib/features/checkout/binding/checkout_binding .dart
@@ -1,9 +1,30 @@
+import 'package:fruits_hub/core/repos/orders_repo/orders_repo.dart';
+import 'package:fruits_hub/core/repos/orders_repo/orders_repo_impl.dart';
+import 'package:fruits_hub/core/services/database_service.dart';
+import 'package:fruits_hub/features/checkout/presentation/controller/add_order_controller.dart';
 import 'package:fruits_hub/features/checkout/presentation/controller/checkout_controller.dart';
 import 'package:get/get.dart';
 
 class CheckoutBinding extends Bindings {
   @override
   void dependencies() {
-    Get.put(CheckoutController());
+
+    Get.lazyPut<OrdersRepo>(
+      () => OrdersRepoImpl(
+         Get.find<DatabaseService>(),
+      ),
+    );
+    
+    Get.lazyPut<CheckoutController>(
+      () => CheckoutController()
+      
+      );
+
+    Get.lazyPut<AddOrderController>(
+      () => AddOrderController(
+        ordersRepo: Get.find()
+        )
+        );
+
   }
 }
