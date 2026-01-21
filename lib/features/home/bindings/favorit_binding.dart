ba2a9136081357009519abaf3f068d1ed8f7988e
AbedShehab <abed2003shehab@gmail.com>
2026-01-21 21:00:20 +0200

    refactore code

--- lib/features/home/bindings/favorit_binding.dart
+++ lib/features/home/bindings/favorit_binding.dart
@@ -1,8 +1,8 @@
 import 'package:fruits_hub/core/services/database_service.dart';
 import 'package:fruits_hub/core/services/firestore_service.dart';
 import 'package:fruits_hub/features/home/presentation/controller/favorite_controller.dart';
-import 'package:fruits_hub/features/home/repo/favorite_repo.dart';
-import 'package:fruits_hub/features/home/repo/favorite_repo_impl.dart';
+import 'package:fruits_hub/features/home/domain/repo/favorite_repo.dart';
+import 'package:fruits_hub/features/home/data/repo/favorite_repo_impl.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 class FavoritBinding extends Bindings {
--- lib/features/home/presentation/views/profile_view.dart
+++ lib/features/home/presentation/views/profile_view.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/home/presentation/views/widgets/favorit_view.dart';
+import 'package:get/get.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Get.toNamed(FavoritView.routeName);
+          },
+          child: const Text('الانتقال إلى المفضلة'),
+        ),
+      ),
+    );
   }
 }
