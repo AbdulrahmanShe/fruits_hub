c785ddc26e0791dc22098716d5098349aae2fca4
AbedShehab <abed2003shehab@gmail.com>
2026-01-21 20:53:26 +0200

    Create FavoritBinding

---
+++ lib/features/home/bindings/favorit_binding.dart
@@ -0,0 +1,35 @@
+import 'package:fruits_hub/core/services/database_service.dart';
+import 'package:fruits_hub/core/services/firestore_service.dart';
+import 'package:fruits_hub/features/home/presentation/controller/favorite_controller.dart';
+import 'package:fruits_hub/features/home/repo/favorite_repo.dart';
+import 'package:fruits_hub/features/home/repo/favorite_repo_impl.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+class FavoritBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<DatabaseService>(
+      () => FireStoreService(),
+      fenix: true,
+    );
+
+    Get.lazyPut<FavoriteRepo>(
+      () => FavoriteRepoImpl(Get.find()),
+      fenix: true,
+    );
+
+    Get.lazyPut<FavoriteController>(
+      () {
+        final user = FirebaseAuth.instance.currentUser;
+        if (user == null) {
+          throw Exception('User not logged in');
+        }
+        return FavoriteController(
+          favoriteRepo: Get.find(),
+          userId: user.uid,
+        );
+      },
+      fenix: true,
+    );
+  }
+}
