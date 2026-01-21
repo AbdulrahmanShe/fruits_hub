4860eba51908fb5d6c9baa0e45caee1dff688c1d
AbedShehab <abed2003shehab@gmail.com>
2026-01-21 20:59:56 +0200

    add BackendEndpoint

--- lib/core/utils/backend_endpoint.dart
+++ lib/core/utils/backend_endpoint.dart
@@ -3,5 +3,7 @@ class BackendEndpoint {
   static const getUsersData = 'users';
   static const getProducts = 'products';
   static const isUserExists = 'users';
+  static const users = 'users';
+  static const favorites = 'favorites';
   static const addOrder = 'Orders';
 }
