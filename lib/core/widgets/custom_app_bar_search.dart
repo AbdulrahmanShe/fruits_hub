dd3a02242145e46d3123d2bcc281c78c2414714e
AbedShehab <abed2003shehab@gmail.com>
2026-01-24 00:55:46 +0200

    Create buildAppBarSearch

---
+++ lib/core/widgets/custom_app_bar_search.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/controller/voice_search_controller.dart';
+import 'package:fruits_hub/core/utils/app_colors.dart';
+import 'package:fruits_hub/core/utils/app_text_styles.dart';
+import 'package:get/get.dart';
+
+AppBar buildAppBarSearch({required String title,}) {
+  final controller = Get.find<VoiceSearchController>();
+    return AppBar(
+      backgroundColor: AppColors.white,
+      // elevation: 0,
+      leading: IconButton(
+        onPressed: ()=> Get.back(),
+       icon: Icon(Icons.arrow_back_ios_new),
+       ),
+      title: Center(
+        child: Text(
+          title,
+          style: TextStyles.bold19,
+          ),
+      ),
+        actions: [
+          IconButton(
+            icon: Obx(() => Text(
+              controller.currentLocale.value == 'ar' ? 'AR' : 'EN',
+            )),
+              onPressed: controller.toggleLanguage,
+          ),
+        ],
+    );
+  }
