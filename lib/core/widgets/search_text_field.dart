db8efcffb48aa6f35a41eea7cf8bcf327d62afd8
AbedShehab <abed2003shehab@gmail.com>
2026-01-24 00:59:16 +0200

    convert SearchTextField to StatefulWidget because textEditingController.dispose and add VoiceSearchButton

--- lib/core/widgets/search_text_field.dart
+++ lib/core/widgets/search_text_field.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/utils/app_images.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
+import 'package:fruits_hub/core/widgets/voice_search_button.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/search_view.dart';
 import 'package:svg_flutter/svg.dart';
 import 'package:get/get.dart';
 import 'package:fruits_hub/core/controller/products_controller.dart';
 
 // ignore: must_be_immutable
-class SearchTextField extends StatelessWidget {
+class SearchTextField extends StatefulWidget {
    SearchTextField({super.key,this.readOnly});
   bool? readOnly = false;
 
   @override
+  State<SearchTextField> createState() => _SearchTextFieldState();
+}
+
+class _SearchTextFieldState extends State<SearchTextField> {
+  late final TextEditingController textEditingController;
+  final controller = Get.find<ProductsController>();
+
+  @override
+  void initState() {
+    super.initState();
+    textEditingController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose(); // ✅ ينحذف تلقائي
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final controller = Get.find<ProductsController>();
     return Container(
       decoration: const BoxDecoration(
         boxShadow: [
@@ -26,44 +46,61 @@ class SearchTextField extends StatelessWidget {
         ],
       ),
       child: TextField(
-        onChanged: controller.search,
-    //     onChanged: (value) {
-    //     controller.searchQuery.value = value;
-    //     controller.search(value);
-    //  },
-     readOnly: readOnly ?? false,
-        onSubmitted: controller.addRecentSearch,
-      onTap: () {
-        Get.toNamed(SearchView.routeName);
-      },
-        keyboardType: TextInputType.text,
-        decoration: InputDecoration(
-          prefixIcon: SizedBox(
-            width: 20,
-            child: Center(
-              child: SvgPicture.asset(Assets.imagesSearchIcon),
-            ),
-          ),
-          suffixIcon: SizedBox(
-            width: 20,
-            child: Center(
-              child: SvgPicture.asset(
-                Assets.imagesFilter,
+          controller: textEditingController,
+          onChanged: controller.search,
+          readOnly: widget.readOnly ?? false,
+          onSubmitted: controller.addRecentSearch,
+          onTap: () {
+          if (widget.readOnly == true) {
+              Get.toNamed(SearchView.routeName);
+            }
+          },
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+            prefixIcon: SizedBox(
+              width: 20,
+              child: Center(
+                child: SvgPicture.asset(Assets.imagesSearchIcon),
               ),
             ),
+            /// 🎤 + فلترة
+            suffixIcon: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  /// 🎤 زر الصوت
+                   VoiceSearchButton(
+                    onResult: (text) {
+                      textEditingController.text = text;        // 👈 يظهر فورًا
+                      controller.search(text);
+                    },
+                  ),
+                const SizedBox(width: 8),
+        
+                  /// 🔍 زر الفلترة (كما طلبت)
+                SizedBox(
+                  width: 20,
+                  child: Center(
+                    child: SvgPicture.asset(
+                      Assets.imagesFilter,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+            
+            hintStyle: TextStyles.regular13.copyWith(
+              color: const Color(0xFF949D9E),
+            ),
+            hintText: 'ابحث صوتيًا أو كتابة...',
+            filled: true,
+            fillColor: Colors.grey.shade100, //Colors.white,
+            border: buildBorder(),
+            enabledBorder: buildBorder(),
+            focusedBorder: buildBorder(),
           ),
-          hintStyle: TextStyles.regular13.copyWith(
-            color: const Color(0xFF949D9E),
-          ),
-          hintText: 'ابحث عن...',
-          filled: true,
-          fillColor: Colors.grey.shade100, //Colors.white,
-          border: buildBorder(),
-          enabledBorder: buildBorder(),
-          focusedBorder: buildBorder(),
         ),
-      ),
-    );
+      );
   }
 
   OutlineInputBorder buildBorder() {
