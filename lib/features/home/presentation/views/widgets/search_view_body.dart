a8ceff39b7dade43269ed834d4ba1fe423a39fac
AbedShehab <abed2003shehab@gmail.com>
2026-01-24 00:57:23 +0200

    converte SearchViewBody to StatefulWidget because clearSearch

--- lib/features/home/presentation/views/widgets/search_view_body.dart
+++ lib/features/home/presentation/views/widgets/search_view_body.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/constants.dart';
 import 'package:fruits_hub/core/controller/products_controller.dart';
-import 'package:fruits_hub/core/widgets/custom_app_bar_notification.dart';
+import 'package:fruits_hub/core/widgets/custom_app_bar_search.dart';
 import 'package:fruits_hub/core/widgets/recent_search_list.dart';
 import 'package:fruits_hub/core/widgets/search_results_grid.dart';
 import 'package:fruits_hub/core/widgets/search_text_field.dart';
 import 'package:get/get.dart';
 
-class SearchViewBody extends StatelessWidget {
+class SearchViewBody extends StatefulWidget {
   const SearchViewBody({super.key});
 
   @override
+  State<SearchViewBody> createState() => _SearchViewBodyState();
+}
+
+class _SearchViewBodyState extends State<SearchViewBody> {
+  final controller = Get.find<ProductsController>();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.clearSearch();        // 🔥 هذا هو السر
+  }
+
+  @override
+  void dispose() {
+    controller.clearSearch();        // (اختياري)
+    super.dispose();
+  }
+  
+  @override
   Widget build(BuildContext context) {
-    final controller = Get.find<ProductsController>();
+    
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: CustomScrollView(
@@ -22,10 +41,7 @@ class SearchViewBody extends StatelessWidget {
               children: [
                 const SizedBox(height: kTopPaddding),
 
-                buildAppBarNotification(
-                  title: 'البحث',
-                  showBackBottom: true,
-                ),
+                buildAppBarSearch(title:  'البحث'),
 
                 const SizedBox(height: 16),
                  SearchTextField(),
