4a2fc5533cd2e5b3594a07a248b62af798a8946c
AbedShehab <abed2003shehab@gmail.com>
2026-01-29 15:54:35 +0200

    Create ProductDetailsView

---
+++ lib/features/home/presentation/views/widgets/product_details_view.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/entities/product_entity.dart';
+import 'package:fruits_hub/features/home/presentation/views/widgets/product_details_content.dart';
+import 'package:fruits_hub/features/home/presentation/views/widgets/product_image_section.dart';
+import 'package:get/get.dart';
+
+class ProductDetailsView extends StatelessWidget {
+  const ProductDetailsView({super.key});
+
+  static const routeName = '/product-details';
+
+  @override
+  Widget build(BuildContext context) {
+    final ProductEntity product =
+        Get.arguments as ProductEntity;
+        
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+             ProductImageSection(product: product),
+            Expanded(
+                child: ProductDetailsContent(product: product)
+                
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
