42d960a8bd58081f82b5fe329ec0288d9e80f98b
AbedShehab <abed2003shehab@gmail.com>
2026-02-02 13:30:23 +0200

    Refactore AddToCartSection Bottom and used CartController

--- lib/features/home/presentation/views/widgets/add_to_cart_section.dart
+++ lib/features/home/presentation/views/widgets/add_to_cart_section.dart
@@ -1,24 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/entities/product_entity.dart';
+import 'package:fruits_hub/core/utils/app_colors.dart';
+import 'package:fruits_hub/core/utils/app_text_styles.dart';
+import 'package:fruits_hub/features/home/presentation/controller/cart_controller.dart';
+import 'package:fruits_hub/features/home/presentation/views/cart_view.dart';
+import 'package:get/get.dart';
 
 class AddToCartSection extends StatelessWidget {
-  const AddToCartSection({super.key});
+  const AddToCartSection({super.key, required this.product});
 
+  final ProductEntity product;
+  
   @override
   Widget build(BuildContext context) {
+    final CartController controller = Get.find<CartController>();
     return SizedBox(
       width: double.infinity,
       height: 50,
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: (){
+          if (controller.cartEntity.value.isExis(product)) {
+              Get.to(() => const CartView());
+
+            } else {
+              controller.addProduct(product);
+              Get.to(() => const CartView());
+
+            }
+          },
         style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.green,
+          backgroundColor: AppColors.primaryColor,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
           ),
         ),
-        child: const Text(
+        child: Text(
           'أضف إلى السلة',
-          style: TextStyle(fontSize: 16),
+          style: TextStyles.bold16.copyWith(color: Colors.white),
         ),
       ),
     );
