92321c82737c84f659c738b64b029d2c65ca79f0
AbedShehab <abed2003shehab@gmail.com>
2026-02-04 19:12:44 +0200

    add CategoryItem

---
+++ lib/features/home/presentation/views/widgets/category_item.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.isSelected,
+  });
+
+  final String imageUrl;
+  final String title;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: isSelected ? Colors.green : Colors.transparent,
+              width: 2,
+            ),
+            image: DecorationImage(
+              image: NetworkImage(imageUrl),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        const SizedBox(height: 6),
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+          ),
+        ),
+      ],
+    );
+  }
+}
