e95c2f00087fe72fe6cdd7f46e0bc01a51228c88
AbedShehab <abed2003shehab@gmail.com>
2026-03-06 09:40:21 +0200

    Language integration

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_hub/core/bindings/global_binding.dart';
+import 'package:fruits_hub/core/constants.dart';
 import 'package:fruits_hub/core/services/shared_preferences_singleton.dart';
 import 'package:fruits_hub/features/splash/presentation/views/splash_view.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
@@ -9,11 +10,10 @@ import 'package:fruits_hub/firebase_options.dart';
 import 'generated/l10n.dart';
 import 'package:get/get.dart';
 import 'package:fruits_hub/core/routing/app_pages.dart';
-void main() async{
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Prefs.init();
   runApp(const FruitHub());
 }
@@ -23,31 +23,27 @@ class FruitHub extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final savedLanguage = Prefs.getString(kAppLanguage);
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       initialBinding: GlobalBinding(),
 
       // 🎨 Theme
       theme: ThemeData(
-          fontFamily: 'Cairo',
-          scaffoldBackgroundColor: Colors.white,
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: AppColors.primaryColor,
-          ),
-          ),
+        fontFamily: 'Cairo',
+        scaffoldBackgroundColor: Colors.white,
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
+      ),
 
-
-          // 🌍 Localization (كما هي)
+      // 🌍 Localization (كما هي)
       localizationsDelegates: [
-                S.delegate,
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-                GlobalCupertinoLocalizations.delegate,
-            ],
-            supportedLocales: S.delegate.supportedLocales,
-            locale: const Locale('ar'),
-        
-  
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: Locale(savedLanguage.isEmpty ? 'ar' : savedLanguage),
 
       // 🚦 GetX Routing
       getPages: AppPages.routes,
@@ -55,11 +51,12 @@ class FruitHub extends StatelessWidget {
     );
   }
 }
-    // Folow 
+
+// Folow
 // UI ,
-// Entity , 
-//Repo , Repo Imp , 
-//DatabaseServices , FirebaseServices , 
-//Controller , 
-//Finish Logic, 
+// Entity ,
+//Repo , Repo Imp ,
+//DatabaseServices , FirebaseServices ,
+//Controller ,
+//Finish Logic,
 //Finish Featuers
