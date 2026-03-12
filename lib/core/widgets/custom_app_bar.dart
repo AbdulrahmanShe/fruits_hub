a62b4d66a13c0cf04b20e64b24763787221b8f01
AbedShehab <abed2003shehab@gmail.com>
2026-03-12 10:54:20 +0200

    add theme mode in appBar

--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:fruits_hub/core/utils/app_colors.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 import 'package:get/get.dart';
 
 AppBar buildAppBar(context, {required String title, bool showBackBottom = true,}) {
+    final theme = Theme.of(context);
+    final colors = theme.colorScheme;
     return AppBar(
-      backgroundColor: AppColors.white,
+      backgroundColor: theme.scaffoldBackgroundColor,
       leading: Visibility(
         visible: showBackBottom,
         child: IconButton(
           onPressed: ()=> Get.back(),
-         icon: Icon(Icons.arrow_back_ios_new),
+         icon: Icon(Icons.arrow_back_ios_new, color: colors.onSurface),
          ),
       ),
       centerTitle: true,
       title: Text(
         title,
-        style: TextStyles.bold19,
+        style: TextStyles.bold19.copyWith(color: colors.onSurface),
         ),
     );
   }
