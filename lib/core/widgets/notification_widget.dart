231942ad3281347f302618e38869126622d5b0f2
AbedShehab <abed2003shehab@gmail.com>
2026-03-12 10:58:28 +0200

    add theme in NotificationWidget

--- lib/core/widgets/notification_widget.dart
+++ lib/core/widgets/notification_widget.dart
@@ -9,13 +9,20 @@ class NotificationWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
     return Container(
       padding: EdgeInsets.all(12),
       decoration: ShapeDecoration(
-        color: Color(0xFFEEF8ED),
+        color: colors.primary.withValues(alpha: 0.12),
         shape: OvalBorder()
         ),
-        child: SvgPicture.asset(Assets.imagesNotification),
+        child: SvgPicture.asset(
+          Assets.imagesNotification,
+          colorFilter: ColorFilter.mode(
+            colors.primary,
+            BlendMode.srcIn,
+          ),
+        ),
     );
   }
 }
