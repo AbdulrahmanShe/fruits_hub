01cd143ec4ced66c0e99d9fa2d2c1733b18c576c
AbedShehab <abed2003shehab@gmail.com>
2026-03-14 17:10:45 +0200

    add getOrders in OrdersRepo

--- lib/core/repos/orders_repo/orders_repo.dart
+++ lib/core/repos/orders_repo/orders_repo.dart
@@ -1,8 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:fruits_hub/features/checkout/domain/entites/order_entity.dart';
+import 'package:fruits_hub/features/checkout/domain/entites/order_history_entity.dart';
 
 import '../../errors/failures.dart';
 
 abstract class OrdersRepo {
   Future<Either<Failure, void>> addOrder({required OrderInputEntity order});
+  Future<Either<Failure, List<OrderHistoryEntity>>> getOrders({
+    required String userId,
+  });
 }
