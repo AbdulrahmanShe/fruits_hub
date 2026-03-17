1ac912ed59c36ee649d4eaa9ca7bee4a58684e46
AbedShehab <abed2003shehab@gmail.com>
2026-03-17 14:55:46 +0200

    download packge flutter_stripe and Requirements

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,14 +42,15 @@ android {
             )
         }
     }
-    dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.11.0")
-}
 }
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.11.0")
 }
