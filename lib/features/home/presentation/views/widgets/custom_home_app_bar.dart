d008a6313fae2d0189648189f9c75d59768c756f
AbedShehab <abed2003shehab@gmail.com>
2026-03-27 17:53:08 +0200

    add counter in sala icon in home app bar

--- lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
@@ -1,10 +1,12 @@
 ﻿import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/utils/app_images.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
-import 'package:fruits_hub/core/widgets/notification_widget.dart';
+import 'package:fruits_hub/features/home/presentation/controller/cart_controller.dart';
+import 'package:fruits_hub/features/home/presentation/controller/main_controller.dart';
 import 'package:fruits_hub/features/home/presentation/controller/profile_controller.dart';
 import 'package:fruits_hub/generated/l10n.dart';
 import 'package:get/get.dart';
+import 'package:svg_flutter/svg.dart';
 
 class CustomHomeAppBar extends StatelessWidget {
   const CustomHomeAppBar({super.key});
@@ -12,6 +14,8 @@ class CustomHomeAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final profileController = Get.find<ProfileController>();
+    final cartController = Get.find<CartController>();
+    final mainController = Get.find<MainController>();
 
     return Obx(() {
       final colors = Theme.of(context).colorScheme;
@@ -25,7 +29,64 @@ class CustomHomeAppBar extends StatelessWidget {
               : profileController.userName.value;
 
       return ListTile(
-        trailing: const NotificationWidget(),
+        trailing: InkWell(
+          onTap: () {
+            mainController.setIndex(2);
+          },
+          borderRadius: BorderRadius.circular(999),
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                decoration: ShapeDecoration(
+                  color: colors.primary.withValues(alpha: 0.12),
+                  shape: const OvalBorder(),
+                ),
+                child: SvgPicture.asset(
+                  Assets.imagesVuesaxOutlineShoppingCart,
+                  width: 20,
+                  height: 20,
+                  colorFilter: ColorFilter.mode(
+                    colors.primary,
+                    BlendMode.srcIn,
+                  ),
+                ),
+              ),
+              PositionedDirectional(
+                end: -2,
+                top: -2,
+                child: Obx(() {
+                  final count = cartController.totalItems;
+                  if (count == 0) {
+                    return const SizedBox.shrink();
+                  }
+                  return Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 6,
+                      vertical: 2,
+                    ),
+                    decoration: BoxDecoration(
+                      color: colors.error,
+                      borderRadius: BorderRadius.circular(999),
+                      border: Border.all(
+                        color: colors.surface,
+                        width: 1,
+                      ),
+                    ),
+                    child: Text(
+                      count > 99 ? '99+' : count.toString(),
+                      style: TextStyles.regular13.copyWith(
+                        color: colors.onError,
+                        fontSize: 10,
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ],
+          ),
+        ),
         leading: ClipOval(
           child: Image.asset(
             avatarPath,
