59e02724a5b0269209f88ab37b458172c697a2db
AbedShehab <abed2003shehab@gmail.com>
2026-03-28 13:14:17 +0300

    refactore Binding

--- lib/core/bindings/global_binding.dart
+++ lib/core/bindings/global_binding.dart
@@ -1,10 +1,18 @@
 import 'package:fruits_hub/features/home/presentation/controller/cart_controller.dart';
 import 'package:fruits_hub/features/home/presentation/controller/profile_controller.dart';
+import 'package:fruits_hub/core/services/database_service.dart';
+import 'package:fruits_hub/core/services/firestore_service.dart';
 import 'package:get/get.dart';
 
 class GlobalBinding extends Bindings {
   @override
   void dependencies() {
+    if (!Get.isRegistered<DatabaseService>()) {
+      Get.put<DatabaseService>(
+        FireStoreService(),
+        permanent: true,
+      );
+    }
     Get.put<CartController>(
       CartController(),
       permanent: true,
--- lib/features/checkout/binding/checkout_binding.dart
+++ lib/features/checkout/binding/checkout_binding.dart
@@ -1,6 +1,7 @@
 import 'package:fruits_hub/core/repos/orders_repo/orders_repo.dart';
 import 'package:fruits_hub/core/repos/orders_repo/orders_repo_impl.dart';
 import 'package:fruits_hub/core/services/database_service.dart';
+import 'package:fruits_hub/core/services/firestore_service.dart';
 import 'package:fruits_hub/features/checkout/presentation/controller/add_order_controller.dart';
 import 'package:fruits_hub/features/checkout/presentation/controller/checkout_controller.dart';
 import 'package:get/get.dart';
@@ -8,6 +9,12 @@ import 'package:get/get.dart';
 class CheckoutBinding extends Bindings {
   @override
   void dependencies() {
+    if (!Get.isRegistered<DatabaseService>()) {
+      Get.lazyPut<DatabaseService>(
+        () => FireStoreService(),
+        fenix: true,
+      );
+    }
 
     Get.lazyPut<OrdersRepo>(
       () => OrdersRepoImpl(
