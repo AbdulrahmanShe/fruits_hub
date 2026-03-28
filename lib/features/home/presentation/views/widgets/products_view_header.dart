ad77142eeb04c0bf2b5e30d62dad2dfcd35c1a8d
AbedShehab <abed2003shehab@gmail.com>
2026-03-28 19:44:22 +0300

    refactore

--- lib/features/home/presentation/views/widgets/products_view_header.dart
+++ lib/features/home/presentation/views/widgets/products_view_header.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:fruits_hub/core/utils/app_images.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 import 'package:fruits_hub/generated/l10n.dart';
-import 'package:svg_flutter/svg.dart';
 
 class ProductsViewHeader extends StatelessWidget {
   const ProductsViewHeader({super.key, required this.productsLength});
   final int productsLength;
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
     return Row(
       children: [
         Text(
@@ -18,19 +15,7 @@ class ProductsViewHeader extends StatelessWidget {
           style: TextStyles.bold16,
         ),
         const Spacer(),
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          decoration: ShapeDecoration(
-            color: colors.surface,
-            shape: RoundedRectangleBorder(
-              side: BorderSide(width: 1, color: colors.outline.withValues(alpha: 0.4)),
-              borderRadius: BorderRadius.circular(4),
-            ),
-          ),
-          child: SvgPicture.asset(
-            Assets.imagesFilter2,
-          ),
-        )
+        SizedBox.shrink(),
       ],
     );
   }
