17e012d2ec49c4ad53dfb36eb3f8c0ef5bef60f9
AbedShehab <abed2003shehab@gmail.com>
2026-03-28 20:29:27 +0300

    refactore Theme colore dark

--- lib/features/checkout/presentation/views/widgets/payment_item.dart
+++ lib/features/checkout/presentation/views/widgets/payment_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fruits_hub/core/utils/app_decorations.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 
 class PaymentItem extends StatelessWidget {
@@ -9,19 +8,26 @@ class PaymentItem extends StatelessWidget {
   final Widget child;
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           '$tile:',
-          style: TextStyles.bold13,
+          style: TextStyles.bold13.copyWith(color: colors.onSurface),
         ),
         const SizedBox(
           height: 8,
         ),
         Container(
           padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-          decoration: AppDecorations.greyBoxDecoration,
+          decoration: BoxDecoration(
+            color: colors.surface,
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+              color: colors.outline.withValues(alpha: 0.3),
+            ),
+          ),
           child: child,
         ),
       ],
